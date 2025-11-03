c8b930525b97bbbaaf645836edd060c292c4d13f
Carlos Pedroso <pedrosotechnologia@gmail.com>
2025-11-03 15:54:08 -0300

    Update emailService.js

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -1,32 +1,29 @@
-const nodemailer = require("nodemailer");
+const Mailgun = require("mailgun-js");
 
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: true,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
+const mg = Mailgun({
+  apiKey: process.env.MAILGUN_API_KEY,
+  domain: process.env.MAILGUN_DOMAIN
 });
 
 const sendVerificationEmail = async (to, token) => {
   const verificationLink = `${process.env.URL_SITE}/associado/verificar/${token}`;
 
+  const data = {
+    from: process.env.MAILGUN_FROM, 
+    to,
+    subject: "Confirme seu cadastro - A.E.P.",
+    html: `
+      <h1>Bem-vindo(a) à A.E.P.</h1>
+      <p>Para confirmar seu cadastro, clique no link abaixo:</p>
+      <a href="${verificationLink}">Confirmar Cadastro</a>
+      <br/><br/>
+      <p>Se você não solicitou este cadastro, ignore este e-mail.</p>
+    `
+  };
+
   try {
-    await transporter.sendMail({
-      from: `"A.E.P." <${process.env.SMTP_USER}>`,
-      to,
-      subject: "Confirme seu cadastro - A.E.P.",
-      html: `
-        <h1>Bem-vindo(a) à A.E.P.</h1>
-        <p>Para confirmar seu cadastro, clique no link abaixo:</p>
-        <a href="${verificationLink}">Confirmar Cadastro</a>
-        <br/><br/>
-        <p>Se você não solicitou este cadastro, ignore este e-mail.</p>
-      `,
-    });
-    console.log("✅ E-mail enviado para", to);
+    const body = await mg.messages().send(data);
+    console.log("✅ E-mail enviado para", to, body);
   } catch (err) {
     console.error("❌ Erro ao enviar e-mail:", err);
   }
