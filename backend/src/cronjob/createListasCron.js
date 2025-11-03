f2c745bdf5bca6a1a22ef34036942c15f7dc5b5e
Carlos Pedroso <carlosvp8@gmail.com>
2025-11-03 09:45:32 -0300

    Reapply "Manutenção na Criação das Listas"
    This reverts commit e0496223ba92a9d345d665e70a2883c6e06e10aa.

--- backend/src/cronjob/createListasCron.js
+++ backend/src/cronjob/createListasCron.js
@@ -1,8 +1,17 @@
 const cron = require("node-cron");
-const { criarListasViagem } = require("../controller/listaViagemController");
+const {
+  criarListasViagemMatutino,
+  criarListasViagemNoturno,
+} = require("../controller/listaViagemController");
 
-// Executar todo dia à meia-noite
-cron.schedule("29 09 * * *", () => {
-  console.log("Rodando criação automática de listas...");
-  criarListasViagem();
+// Executar todo dia à meia-noite de Domingo a Quinta-Feira
+cron.schedule("0 0 * * 0-4", () => {
+  console.log("Rodando criação automática de listas MATUTINO...");
+  criarListasViagemMatutino();
+});
+
+// Executar todo dia à meia-noite de Segunda a Sexta-Feira
+cron.schedule("0 0 * * 1-5", () => {
+  console.log("Rodando criação automática de listas NOTURNO...");
+  criarListasViagemNoturno();
 });
